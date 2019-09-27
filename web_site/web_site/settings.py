7ad42b5eadf057c42f0cd6cc1c14ec7021eb2249
SatelliteCat <dmn.info@mail.ru>
2019-09-27 23:11:22 +0400

    Init project

--- web_site/web_site/settings.py
+++ web_site/web_site/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'web_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_db',
+        'USER': 'postgres',
+        'PASSWORD': '1',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
